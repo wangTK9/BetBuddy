5cb0a5018622df9a8dd992afcaec3f9b1661cd17
ThoaiDev39 <31.lethoai.9.10@gmail.com>
2025-02-17 12:28:00 +0700

    update css login vs userprofile

--- client/src/components/ChatBox.vue
+++ client/src/components/ChatBox.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="chat-container">
     <div class="chat-header">
-      <h2>Chat với {{ receiver }}</h2>
+      <h2>{{ receiver && receiver.length > 20 ? receiver.substring(0, 20) + "..." : receiver || "Người dùng" }}</h2>
+      <div class="icons">
+        <i class="fas fa-phone icon"></i>
+        <i class="fas fa-video icon"></i>
+        <i class="fas fa-user icon"></i>
+        <i class="fas fa-ellipsis-h icon"></i>
+      </div>
     </div>
 
     <div class="chat-messages">
-      <div
-        v-for="msg in messages"
-        :key="msg._id"
-        :class="msg.sender === userId ? 'sent' : 'received'"
-      >
+      <div v-for="msg in messages" :key="msg._id" :class="msg.sender === userId ? 'sent' : 'received'">
+        <div v-if="searchedUser" class="searched-user" @click="selectUser(searchedUser)">
+          <img :src="'/avatar-trang-4.jpg'" :alt="searchedUser.fullName" class="searched-user-avatar" />
+        </div>
         <p>{{ msg.message }}</p>
       </div>
     </div>
 
     <div class="chat-input">
-      <input
-        v-model="message"
-        placeholder="Nhập tin nhắn..."
-        @keyup.enter="sendMessage"
-      />
+      <input v-model="message" placeholder="Nhập tin nhắn..." @keyup.enter="sendMessage" />
+      <i class="fas fa-smile icon"></i>
+      <i class="fas fa-paperclip icon"></i>
       <button @click="sendMessage">Gửi</button>
     </div>
   </div>
@@ -35,10 +38,13 @@ export default {
     return {
       message: "",
       messages: [],
+      searchedUser: null,
+      searchQuery: "",
       socket: null,
       backendUrl: "http://localhost:5000",
     };
   },
+  
   async created() {
     this.socket = io(this.backendUrl);
 
@@ -60,6 +66,22 @@ export default {
     await this.loadMessages();
   },
   methods: {
+    async searchUser() {
+      if (!this.searchQuery) {
+        this.searchedUser = null;
+        return;
+      }
+      try {
+        const response = await axios.get(`http://localhost:5000/api/user/search?email=${this.searchQuery}`);
+        this.searchedUser = response.data.length > 0 ? response.data[0] : null;
+      } catch (error) {
+        console.error("User not found", error);
+        this.searchedUser = null;
+      }
+    },
+    selectUser(user) {
+      this.$emit("user-selected", user);
+    },
     async loadMessages() {
       try {
         const res = await axios.get(
@@ -102,38 +124,186 @@ export default {
 
 <style>
 .chat-container {
-  width: 300px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 10px;
-  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #f0f0f0;
+  font-family: Arial, sans-serif;
+  margin: 0;
+  width: 100%;
+  max-width: 700px;
+  height: 600px;
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  animation: fadeIn 0.5s ease-in-out;
 }
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+    transform: scale(0.9);
+  }
+
+  to {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+
 .chat-header {
-  text-align: center;
+  display: flex;
+  width: 700px;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: #fff;
+  color: #7c3aed;
+  font-size: 16px;
   font-weight: bold;
+  border-bottom: 2px solid #7c3aed;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
+
+.chat-header h2 {
+  flex-grow: 1;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  margin-right: 10px;
+}
+
+.chat-header .icons {
+  display: flex;
+  align-items: center;
+}
+
+.chat-header .icons .icon {
+  font-size: 20px;
+  color: #7c3aed;
+  margin-left: 10px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+.chat-header .icons .icon:hover {
+  color: #6b2fcf;
+}
+
 .chat-messages {
-  height: 300px;
+  flex-grow: 1;
+  padding: 20px;
   overflow-y: auto;
-  padding: 10px;
+  width: 700px;
+  background-color: #f9f9f9;
 }
+
 .sent {
-  text-align: right;
-  color: blue;
+  margin-bottom: 2px;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+  animation: slideIn 0.3s ease-in-out;
 }
+
+.sent p {
+  max-width: 70%;
+  padding: 10px 15px;
+  border-radius: 20px;
+  font-size: 14px;
+  line-height: 1.4;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  background-color: #7c3aed;
+  color: #fff;
+  border-bottom-right-radius: 0;
+}
+
+@keyframes slideIn {
+  from {
+    opacity: 0;
+    transform: translateX(-10px);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
 .received {
-  text-align: left;
-  color: green;
+  margin-bottom: 2px;
+  display: flex;
+  align-items: flex-end;
+  animation: slideIn 0.3s ease-in-out;
+  justify-content: flex-start;
 }
+
+.received p {
+  max-width: 70%;
+  padding: 10px 15px;
+  border-radius: 20px;
+  font-size: 14px;
+  line-height: 1.4;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  background-color: #ffccf9;
+  color: #000;
+  border-bottom-left-radius: 0;
+}
+
 .chat-input {
   display: flex;
-  gap: 5px;
+  width: 700px;
+  align-items: center;
+  padding: 10px;
+  border-top: 1px solid #ccc;
+  background-color: #fff;
+  box-shadow: 0 -2px 4px rgba(0, 0, 0, 0.1);
 }
+
 .chat-input input {
-  flex: 1;
-  padding: 5px;
+  flex-grow: 1;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 20px;
+  font-size: 14px;
+  margin-right: 10px;
+  transition: border-color 0.3s;
 }
+
+.chat-input input:focus {
+  border-color: #7c3aed;
+  outline: none;
+}
+
 .chat-input button {
-  padding: 5px;
+  padding: 10px 20px;
+  background-color: #7c3aed;
+  color: #fff;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+  font-size: 14px;
+  margin-right: 10px;
+  transition: background-color 0.3s;
+}
+
+.chat-input button:hover {
+  background-color: #6b2fcf;
+}
+
+.chat-input .icon {
+  font-size: 20px;
+  color: #7c3aed;
+  margin-right: 10px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+.chat-input .icon:hover {
+  color: #6b2fcf;
 }
 </style>
--- client/src/components/ChatList.vue
+++ client/src/components/ChatList.vue
@@ -1,54 +1,65 @@
 <template>
   <div class="chat-container">
-    <!-- Header -->
-    <div class="header">
-      <span>Chats</span>
-      <span>👤</span>
-    </div>
+    <div class="card">
+      <div class="card-header">
+        <h2>Chats</h2>
+        <i class="fas fa-user-plus add-member-icon" @click="openAddMemberModal"></i>
+      </div>
 
-    <!-- Search Bar -->
-    <input
-      type="text"
-      class="search-bar"
-      placeholder="Search messages or users"
-      v-model="searchQuery"
-      @input="searchUser"
-    />
-
-    <!-- Hiển thị user tìm thấy -->
-    <div
-      v-if="searchedUser"
-      class="searched-user"
-      @click="selectUser(searchedUser)"
-    >
-      <img
-        :src="'/avatar-trang-4.jpg'"
-        :alt="searchedUser.fullName"
-        class="searched-user-avatar"
-      />
-      <p class="searched-user-name">{{ searchedUser.fullName }}</p>
-    </div>
-
-    <!-- Active Users -->
-    <div class="active-users">
-      <!-- <div class="active-user" v-for="user in activeUsers" :key="user.name">
-        <img :src="user.image" :alt="user.name" />
-        <div class="online-status"></div>
-      </div> -->
-    </div>
-
-    <!-- Recent Chats -->
-    <h3>Recent</h3>
-    <div class="chat-list">
-      <div class="chat-item" v-for="chat in filteredChats" :key="chat.name">
-        <div class="chat-avatar" :style="{ backgroundColor: chat.color }">
-          {{ chat.initial }}
+      <!-- Modal Add Member -->
+      <!-- Modal -->
+      <div v-if="showAddMemberModal" class="modal-overlay">
+        <div class="modal">
+          <div class="modal-header">
+            <h2>Add Member</h2>
+            <button @click="closeAddMemberModal">✖</button>
+          </div>
+          <div class="modal-body">
+            <input v-model="newMemberEmail" type="email" placeholder="Enter name" />
+          </div>
+          <div class="modal-footer">
+            <button class="btn-cancel" @click="closeAddMemberModal">Close</button>
+            <button class="btn-save" @click="addMember">OK</button>
+          </div>
         </div>
-        <div class="chat-info">
-          <p class="chat-name">{{ chat.name }}</p>
-          <p class="chat-message">{{ chat.message }}</p>
+      </div>
+
+      <div class="search-box">
+        <input type="text" placeholder="Search messages or users" v-model="searchQuery" @input="searchUser" />
+        <i class="fas fa-search"></i>
+      </div>
+      <div v-if="searchedUser" class="searched-user" @click="selectUser(searchedUser)">
+        <img :src="'/avatar-trang-4.jpg'" :alt="searchedUser.fullName" class="searched-user-avatar" />
+        <p class="searched-user-name">{{ searchedUser.fullName }}</p>
+      </div>
+      <div class="active-users">
+        <h2>Active Users</h2>
+        <div class="user-list">
+          <div class="user" v-for="user in activeUsers" :key="user.name">
+            <div class="avatar">{{ user.initial }}</div>
+            <div class="name">{{ user.name }}</div>
+          </div>
         </div>
-        <span class="chat-time">{{ chat.time }}</span>
+      </div>
+      <div class="recent-title">
+        <h2>Recent</h2>
+      </div>
+      <div class="recent-item">
+        <div class="chat-list">
+          <div class="chat-item" v-for="chat in filteredChats" :key="chat.name">
+            <div class="chat-avatar" :style="{ backgroundColor: chat.color }">{{ chat.initial }}</div>
+            <div class="chat-info">
+              <p class="chat-name">{{ chat.name }}</p>
+              <p class="chat-message">{{ chat.message }}</p>
+            </div>
+            <span class="chat-time">{{ chat.time }}</span>
+            <div class="icons">
+              <i class="fas fa-comment-dots"></i>
+              <i class="fas fa-phone"></i>
+              <i class="fas fa-video"></i>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -56,36 +67,27 @@
 
 <script>
 import axios from "axios";
-import { useAuthStore } from "../stores/auth";
-import { defineComponent } from "vue";
 
-export default defineComponent({
+export default {
   data() {
     return {
       searchQuery: "",
-      searchedUser: null, // Lưu thông tin user tìm thấy
-      loading: false, // Trạng thái loading khi tìm kiếm
-      activeUsers: [],
+      searchedUser: null,
+      activeUsers: [
+        { name: "Alice", initial: "A" },
+        { name: "Bob", initial: "B" },
+        { name: "Charlie", initial: "C" }
+      ],
       chats: JSON.parse(localStorage.getItem("recentChats")) || [],
+      showAddMemberModal: false,
+      newMemberEmail: "",
+      newMemberName: "", // Thêm state để nhập tên thành viên
     };
   },
   computed: {
     filteredChats() {
-      return this.chats.filter(
-        (chat) =>
-          chat.name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-          chat.message.toLowerCase().includes(this.searchQuery.toLowerCase())
-      );
-    },
-  },
-  watch: {
-    searchQuery(newQuery) {
-      if (newQuery.trim() === "") {
-        this.searchedUser = null;
-        return;
-      }
-      this.searchUser();
-    },
+      return this.chats.filter(chat => chat.name.toLowerCase().includes(this.searchQuery.toLowerCase()));
+    }
   },
   methods: {
     async searchUser() {
@@ -94,177 +96,489 @@ export default defineComponent({
         return;
       }
       try {
-        const response = await axios.get(
-          `http://localhost:5000/api/user/search?email=${this.searchQuery}`
-        );
-        console.log("📥 API Response:", response.data); // Debug log
-        this.searchedUser = response.data.length > 0 ? response.data[0] : null; // Lấy user đầu tiên
-
-        if (this.searchedUser) {
-          this.addToRecentChats(this.searchedUser);
-        }
+        const response = await axios.get(`http://localhost:5000/api/user/search?email=${this.searchQuery}`);
+        this.searchedUser = response.data.length > 0 ? response.data[0] : null;
       } catch (error) {
         console.error("User not found", error);
         this.searchedUser = null;
       }
     },
-    addToRecentChats(user) {
-      const newChat = {
-        name: user.fullName,
-        message: "Recently searched",
-        time: new Date().toLocaleDateString(),
-        color: "#fbbf24",
-        initial: user.fullName.charAt(0).toUpperCase(),
-      };
-      // Remove any existing chat with the same name
-      this.chats = this.chats.filter((chat) => chat.name !== user.fullName);
-      // Add the new chat to the beginning of the array
-      this.chats.unshift(newChat);
-      // Save to local storage
-      localStorage.setItem("recentChats", JSON.stringify(this.chats));
-    },
     selectUser(user) {
-      const authStore = useAuthStore();
-      authStore.setSelectedUserWalletAddress(user.walletAddress);
-      console.log(
-        "Selected User Wallet Address:",
-        authStore.selectedUserWalletAddress
-      ); // Debug log
-      console.log("My Wallet Address:", authStore.walletAddress); // Debug log
       this.$emit("user-selected", user);
     },
-  },
-});
+    openAddMemberModal() {
+      this.showAddMemberModal = true;
+    },
+    closeAddMemberModal() {
+      this.showAddMemberModal = false;
+      this.newMemberEmail = "";
+      this.newMemberName = "";
+    },
+    async addMember() {
+      if (this.newMemberEmail.trim() === "" || this.newMemberName.trim() === "") {
+        alert("Vui lòng nhập đầy đủ tên và email!");
+        return;
+      }
+
+      try {
+        const response = await axios.post("http://localhost:5000/api/chat/addMember", {
+          email: this.newMemberEmail
+        });
+
+        if (response.data.success) {
+          this.activeUsers.push({
+            name: this.newMemberName,
+            initial: this.newMemberName.charAt(0).toUpperCase()
+          });
+          alert("Thành viên đã được thêm!");
+        } else {
+          alert("Không thể thêm thành viên. Hãy kiểm tra lại!");
+        }
+      } catch (error) {
+        console.error("Lỗi khi thêm thành viên:", error);
+        alert("Đã xảy ra lỗi!");
+      }
+
+      this.closeAddMemberModal();
+    }
+  }
+};
 </script>
+
 
 <style scoped>
 .chat-container {
-  width: 100%;
-  padding: 15px;
-  background-color: #f9f9f9;
-  border-radius: 10px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  display: flex;
+  min-height: 100vh;
+  width: 470px;
+  background-color: #f3f4f6;
+  padding: 16px;
 }
 
-.header {
+.card {
+  background-color: white;
+  border-radius: 12px;
+  box-shadow: 0 10px 15px rgba(0, 0, 0, 0.1);
+  padding: 24px;
+  width: 100%;
+  max-width: 470px;
+}
+
+.card-header {
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+
+.card-header h2 {
+  color: #7c3aed;
   font-size: 24px;
-  font-weight: bold;
-  color: #333;
+}
+
+.card-header i {
+  color: #7c3aed;
+  font-size: 24px;
+}
+
+/* Hiệu ứng xuất hiện modal */
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+    transform: translateY(-6px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+/* Overlay nền mờ khi mở modal */
+.modal-overlay {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.3);
+  backdrop-filter: blur(5px);
+  z-index: 1000;
+}
+
+/* Modal */
+.modal {
+  background: white;
+  border-radius: 10px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  width: 700px;
+  height: 200px;
+  animation: fadeIn 0.3s ease-out;
+}
+
+/* Header */
+.modal-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #ddd;
+  padding-bottom: 10px;
   margin-bottom: 15px;
 }
 
-.search-bar {
+.modal-header h2 {
+  font-size: 22px;
+  color: #7c3aed;
+  font-weight: bold;
+  margin: 0;
+}
+
+.modal-header button {
+  background: none;
+  border: none;
+  font-size: 18px;
+  cursor: po  inter;
+  color: #888;
+}
+
+.modal-header button:hover {
+  color: #333;
+}
+
+/* Input field */
+.modal-body input {
+  width: 630px;
+  padding: 15px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 14px;
+  transition: border 0.3s;
+}
+
+.modal-body input:focus {
+  border: 1px solid #5b73e8;
+  outline: none;
+}
+
+/* Footer */
+.modal-footer {
+  display: flex;
+  justify-content: flex-end;
+  gap: 10px;
+}
+
+.modal-footer button {
+  padding: 8px 15px;
+  font-size: 14px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.btn-cancel {
+  background: none;
+  color: #7c3aed;
+}
+
+.btn-cancel:hover {
+  text-decoration: underline;
+}
+
+.btn-save {
+  background: #7c3aed;
+  color: #fff;
+}
+
+.btn-save:hover {
+  background: #3d5ad6;
+}
+
+
+.search-box {
+  margin-bottom: 16px;
+  position: relative;
   width: 100%;
-  padding: 12px;
-  margin-bottom: 15px;
-  border-radius: 8px;
-  border: 1px solid #ddd;
+  display: flex;
+  align-items: center;
+  background-color: #fff;
+  border: 1px solid #d1d5db;
+  border-radius: 25px;
+  padding: 8px 16px;
+  transition: all 0.3s ease-in-out;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.search-box input {
+  flex: 1;
+  border: none;
   outline: none;
   font-size: 16px;
+  padding: 10px;
+  background: transparent;
+}
+
+.search-box input::placeholder {
+  color: #9ca3af;
+}
+
+.search-box i {
+  color: #7c3aed;
+  font-size: 20px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+.search-box:hover {
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.15);
+}
+
+.search-box input:focus {
+  border: none;
+  outline: none;
+  box-shadow: 0 0 5px rgba(124, 58, 237, 0.5);
 }
 
 .searched-user {
   display: flex;
   align-items: center;
-  background: #fff;
+  background: #f9fafb;
   padding: 10px;
-  margin-bottom: 15px;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 12px;
   cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  border: 1px solid #e5e7eb;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
+}
+
+.searched-user:hover {
+  background: #ede9fe;
+  transform: scale(1.02);
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 
 .searched-user-avatar {
-  width: 40px;
-  height: 40px;
+  width: 50px;
+  height: 50px;
   border-radius: 50%;
-  margin-right: 10px;
+  object-fit: cover;
+  margin-right: 12px;
+  border: 2px solid #7c3aed;
 }
 
 .searched-user-name {
   font-size: 16px;
-  font-weight: bold;
-  color: #333;
+  font-weight: 600;
+  color: #4b5563;
 }
+
 
 .active-users {
+  margin-bottom: 16px;
+}
+
+.active-users h2 {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 16px;
+  color: #7c3aed;
+}
+
+.active-users .user-list {
   display: flex;
-  gap: 10px;
-  margin-bottom: 15px;
+  overflow-x: auto;
+  gap: 16px;
 }
 
-.active-user {
-  position: relative;
+.active-users .user {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  animation: fadeIn 1s ease-in-out;
 }
 
-.active-user img {
+.active-users .avatar {
   width: 40px;
   height: 40px;
   border-radius: 50%;
-  border: 2px solid #4caf50;
+  background-color: #7c3aed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  position: relative;
+  transition: transform 0.3s;
 }
 
-.online-status {
+.active-users .avatar:hover {
+  transform: scale(1.1);
+}
+
+.active-users .avatar::after {
+  content: '';
+  width: 10px;
+  height: 10px;
+  background-color: #34d399;
+  border: 2px solid white;
+  border-radius: 50%;
   position: absolute;
   bottom: 0;
   right: 0;
-  width: 10px;
-  height: 10px;
-  background-color: #4caf50;
+}
+
+.active-users .name {
+  font-weight: 600;
+  color: #4b5563;
+  margin-top: 4px;
+  font-size: 14px;
+}
+
+/* 
+.user-list {
+  display: flex;
+  gap: 10px;
+  overflow-x: auto;
+}
+
+.user {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.avatar {
+  width: 40px;
+  height: 40px;
+  background-color: #7c3aed;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   border-radius: 50%;
-  border: 2px solid #fff;
+} */
+
+.recent-title {
+  font-size: 18px;
+  margin-bottom: 8px;
+  color: #7c3aed;
+}
+
+.recent-item {
+  display: flex;
+  align-items: center;
+  padding: 16px;
+  background-color: #f9fafb;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
+  margin-bottom: 16px;
+  transition: transform 0.3s, box-shadow 0.3s;
+  animation: fadeIn 1s ease-in-out;
+}
+
+.recent-item:hover {
+  transform: translateY(-2px);
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.recent-item:last-child {
+  margin-bottom: 0;
+}
+
+.recent-item .chat-list .chat-item .chat-avatar {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: #7c3aed !important;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-weight: bold;
+  margin-right: 16px;
+  font-size: 18px;
+}
+
+.recent-item .chat-info {
+  flex: 1;
+}
+
+.recent-item .chat-info .chat-name {
+  font-weight: 600;
+  color: #4b5563;
+}
+
+.recent-item .chat-info .chat-time {
+  font-size: 12px;
+  color: #6b7280;
+}
+
+.recent-item .chat-info .chat-message {
+  font-size: 14px;
+  color: #6b7280;
+}
+
+.recent-item .icons {
+  display: flex;
+  gap: 8px;
+  margin-left: 15px;
+}
+
+.recent-item .icons i {
+  color: #6b7280;
+  font-size: 18px;
+  transition: color 0.3s;
+}
+
+.recent-item .icons i:hover {
+  color: #7c3aed;
 }
 
 .chat-list {
-  background: #fff;
-  padding: 15px;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  margin-top: 16px;
 }
 
 .chat-item {
   display: flex;
   align-items: center;
-  padding: 10px 0;
-  border-bottom: 1px solid #eee;
-}
+  padding: 10px;
+  background-color: #f9fafb;
+  border-radius: 8px;
+  margin-bottom: 8px;
 
-.chat-item:last-child {
-  border-bottom: none;
 }
 
 .chat-avatar {
   width: 40px;
   height: 40px;
-  border-radius: 50%;
   display: flex;
-  justify-content: center;
   align-items: center;
-  color: #fff;
-  font-weight: bold;
-  margin-right: 10px;
+  justify-content: center;
+  border-radius: 50%;
+  color: white;
+  font-size: 18px;
 }
 
 .chat-info {
-  flex-grow: 1;
+  flex: 1;
+  margin-left: 12px;
 }
 
 .chat-name {
-  font-size: 16px;
   font-weight: bold;
-  color: #333;
+  color: #4b5563;
 }
 
 .chat-message {
   font-size: 14px;
-  color: #666;
+  color: #6b7280;
 }
 
 .chat-time {
   font-size: 12px;
-  color: #999;
+  color: #9ca3af;
 }
 </style>
